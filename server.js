ec3bd1f1728c225c5d224a0127cf50fe0e12390e
Chad Crabtree <chadcrabtree@Chads-MacBook-Pro.local>
2017-08-17 13:56:50 -0400

    WIP

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+const mustacheExpress = require("mustache-express");
+
+const data = require("./data");
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
+app.use(express.static("public"));
+
+app.get("/", function (request, respond) {
+  respond.send(data);
+});
+
+app.get("/index", function (request, respond) {
+  respond.render("index", {users:data.users});
+});
+
+app.get("/index/:name", function (request, respond) {
+  let chosenUser = {};
+  for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].name === request.params.name) {
+      chosenUser = data.users[i];
+    }
+  }
+  respond.render("singleUser", chosenUser);
+})
+
+app.listen(3000, function () {
+  console.log("Successfully started on server 3000");
+});
